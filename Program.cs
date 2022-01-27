5b7d8f95c602d4fe47d8ee8bad4a356d435ca51e
CasperStaahl <cstahl20@student.aau.dk>
2022-01-27 22:44:23 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Compass compass = new Compass();
+        View view = new View();
+        Controller controller = new Controller(view, compass);
+        controller.Run();
+    }
+}
